122cb31254c995766cb84938f89f35bc2d55f253
Morrin-K <karimimorrin95@gmail.com>
2024-06-03 12:05:13 +0300

    incometypes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
 
     production: false,
-    apiUrl: 'http://192.168.89.139:8000/api/v1/', // Your development API URL
+    apiUrl: 'http://192.168.89.193:8000/api/v1/', // Your development API URL
   };
