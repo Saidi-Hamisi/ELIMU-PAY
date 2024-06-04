37993ddb931151f07658cb65e4f4e025bd5cf971
Morrin-K <karimimorrin95@gmail.com>
2024-06-04 15:42:19 +0300

    nothings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
 
     production: false,
-    apiUrl: 'http://192.168.89.193:8000/api/v1/', // Your development API URL
+    apiUrl: 'ttp://192.168.1.163:8000/api/v1/', // Your development API URL
   };
