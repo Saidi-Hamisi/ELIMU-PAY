6f6f97d2ad96a60ddcaa42a7309d76db53f4ada3
Saidi Hamisi <saidihamisi370@mail.com>
2024-06-05 12:49:29 +0300

    5th June

--- src/app/Modules/management/student/student.service.ts
+++ src/app/Modules/management/student/student.service.ts
@@ -1,40 +1,47 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
-  private studentsBaseUrl = `${environment.apiUrl}students/`;
-
   constructor(private _http: HttpClient) { }
+
+
+  serverUrl:string = 'http://127.0.0.1:8000//api/v1';
+
 
   addStudent(data: any): Observable<any> {
     console.log('saving student');
-    const url = `${this.studentsBaseUrl}students/create/`;
-    return this._http.post(url, data);
+    const url = `${this.serverUrl}/students/students/create/`
+    return this._http.post(url,data);
+
   }
 
 
   updateStudent(id: number, data: any): Observable<any> {
     console.log('saving student');
     console.log('data', data);
-    const url = `${this.studentsBaseUrl}${id}/`;
-    return this._http.put<any>(url, data);
+    //http://192.168.90.105:8003/api/v1/students/2/
+
+    const url = `${this.serverUrl}/students/${id}/`
+    return this._http.put<any>(url,data);
   }
 
   getStudentList(): Observable<any> {
-    return this._http.get(`${this.studentsBaseUrl}?page=1`);
+
+    return this._http.get(`${this.serverUrl}/students/?page=1`);
+
+ 
   }
 
-  deleterecord(url: string): Observable<any> {
+  deleterecord(url:string): Observable<any> {
     return this._http.delete(url);
   }
 
   getStudentDetails(uniqueId: string): Observable<any> {
-    return this._http.get(`${this.studentsBaseUrl}${uniqueId}`);
+    return this._http.get(`${this.serverUrl}}/students/${uniqueId}`);
   }
 
   //http://127.0.0.1:8000/api/v1/students/student/list_student_virtual_account?page=1
