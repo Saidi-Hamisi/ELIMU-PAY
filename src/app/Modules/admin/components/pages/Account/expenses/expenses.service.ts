a55f7350cea55e05b86a64c334eef4f813bbe90d
Tenta <paulpapaiyo@gmail.com>
2024-06-07 08:17:14 +0300

    SUP

--- src/app/Modules/admin/components/pages/Account/expenses/expenses.service.ts
+++ src/app/Modules/admin/components/pages/Account/expenses/expenses.service.ts
@@ -7,35 +7,36 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class ExpensesService {
-  private baseUrl = `${environment.apiUrl}expenses/expenses/`;
+  private baseUrl = environment.apiUrl + 'expenses/expenses/';
 
   constructor(private _http: HttpClient) {}
-header =new HttpHeaders ().set ('Content-Type','application/json')
+
+  header = new HttpHeaders().set('Content-Type', 'application/json');
+
   // For creating a new expense
   addExpenses(data: any): Observable<any> {
-    // Replace the endpoint with your actual API endpoint
-    console.log("data", data);
-    
-    const url = `${this.baseUrl}`;
-    return this._http.post<any>(url, data ,{headers : this.header });
-    
+    const url = this.baseUrl;
+    return this._http.post<any>(url, data, { headers: this.header });
   }
    
   // For updating an existing expense by ID
   updateExpenses(id: number, data: any): Observable<any> {
-    const url = `${this.baseUrl}${id}/`;
+    if (!id) {
+      throw new Error('Expense ID is required for updating.');
+    }
+    const url = this.baseUrl + id + '/';
     return this._http.put<any>(url, data);
   }
 
   // For fetching the list of expenses
   getExpensesList(): Observable<any> {
-    const url = `${this.baseUrl}`
+    const url = this.baseUrl;
     return this._http.get<any>(url);
   }
    
   // For deleting an expense by ID
   deleteExpenses(id: number): Observable<any> {
-    const url = `${this.baseUrl}${id}/`;
+    const url = this.baseUrl + id + '/';
     return this._http.delete<any>(url);
   }
 }
