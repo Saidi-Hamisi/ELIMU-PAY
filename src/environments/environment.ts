eb9bbd77faf7c6f80f18a90857b50a6ed9d7f12d
Saidi Hamisi <saidihamisi370@mail.com>
2024-06-14 16:21:57 +0300

    users

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://192.168.90.20:8000/api/v1/', // Your development API URL
+    apiUrl: 'http://192.168.88.202:8001/api/v1/', // Your development API URL
   };
