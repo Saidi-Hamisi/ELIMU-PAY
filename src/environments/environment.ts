8cd64895de5efa81e020aa1873f63082b107fc45
ngina-m7 <annmusyoki.ng@gmail.com>
2024-06-19 12:01:27 +0300

    sidebar changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://192.168.90.20:8000/api/v1/', // Your development API URL
+    apiUrl: 'http://192.168.90.83:8000/api/v1/', // Your development API URL
   };
