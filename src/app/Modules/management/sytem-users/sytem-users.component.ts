e51758a71ca8c9146d102f1c065ecb180da4ba51
ngina-m7 <annmusyoki.ng@gmail.com>
2024-06-20 11:47:42 +0300

    sidebar changes

--- src/app/Modules/management/sytem-users/sytem-users.component.ts
+++ src/app/Modules/management/sytem-users/sytem-users.component.ts
@@ -1,15 +1,15 @@
-import html2canvas from 'html2canvas';
-import jspdf from 'jspdf';
-
-import { AddSystemUserComponent } from './add-system-user/add-system-user.component';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { CoreService } from '../../../../@Core/core/core.service';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import * as XLSX from 'xlsx'; // Import xlsx library for Excel export
+
+import { AddSystemUserComponent } from './add-system-user/add-system-user.component';
 import { UserService } from './user.service';
+import { CoreService } from '../../../../@Core/core/core.service';
 
 @Component({
   selector: 'app-sytem-users',
@@ -17,90 +17,12 @@ import { UserService } from './user.service';
   styleUrls: ['./sytem-users.component.css'],
 })
 export class SytemUsersComponent implements OnInit {
-
-  generatePdf() {
-    // Get the table-container div
-    const tableContainer = document.querySelector('.table-container');
-
-    // Check if the table-container div exists
-    if (tableContainer) {
-        // Convert the content of table-container to a canvas
-        html2canvas(tableContainer as HTMLElement).then((canvas) => {
-            // Convert canvas to PDF using jsPDF with landscape orientation
-            const doc = new jspdf('l', 'mm', 'a4'); // 'l' for landscape
-            const imgData = canvas.toDataURL('image/png');
-            const imgWidth = 280; // Adjusted A4 width in mm with margins
-            const pageHeight = 210; // A4 height in mm (landscape orientation)
-            const imgHeight = (canvas.height * imgWidth) / canvas.width;
-            let heightLeft = imgHeight;
-            let position = 10; // Adjust margin top here
-            const marginLeft = 5; // Adjust left margin
-            const marginRight = 3; // Adjust right margin
-
-            function addHeader() {
-                doc.setFontSize(12);
-                doc.text('System Users Report', imgWidth / 2, position, { align: 'center' }); // Header text position
-
-                // Get current date and time
-                const now = new Date();
-                const day = now.toLocaleDateString('en-US', { weekday: 'long' }); // Get the full name of the day
-                const date = now.toLocaleDateString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
-                const time = now.toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit' });
-
-                // Add printed date and time
-                doc.setFontSize(6);
-                doc.text(`Printed on ${day} ${date} at ${time}`, imgWidth - marginRight, position, { align: 'right' });
-            }
-
-            function addFooter() {
-                doc.setFontSize(8);
-                doc.text('Elimu Pay Technologies | Copyright © 2024 | All rights reserved.',
-                    imgWidth / 2, // X position (center)
-                    doc.internal.pageSize.getHeight() - 5, // Y position (5mm from bottom)
-                    { align: 'center' }
-                );
-            }
-
-            addHeader(); // Add header to the first page
-            addFooter(); // Add footer to the first page
-
-            doc.addImage(imgData, 'PNG', marginLeft, position + 10, imgWidth - marginLeft - marginRight, imgHeight); // Adjust position to make space for header and printed date/time
-            heightLeft -= pageHeight;
-
-            while (heightLeft >= 0) {
-                position = heightLeft - imgHeight + 10; // Adjust margin top here
-                doc.addPage();
-                addHeader(); // Add header to subsequent pages
-                addFooter(); // Add footer to subsequent pages
-                doc.addImage(imgData, 'PNG', marginLeft, position + 20, imgWidth - marginLeft - marginRight, imgHeight); // Adjust position to make space for header and printed date/time
-                heightLeft -= pageHeight;
-            }
-
-            // Save the PDF
-            doc.save('System Users Report.pdf');
-        });
-    } else {
-        console.error('System users reports not available');
-    }
-}
-
-
-
-
-
-
-
   displayedColumns: string[] = [
     'id',
     'first_name',
-    // 'middle_name',
     'last_name',
     'usergroup',
-    // 'phoneNumber',
     'email',
-    // 'idNumber',
-    // 'gender',
-    // 'date_of_birth',
     'school_name',
     'address',
     'nationality',
@@ -130,16 +52,11 @@ export class SytemUsersComponent implements OnInit {
         }
       },
     });
-    
   }
-
-  u = []
-
 
   getSystemUserList() {
     this._addSystemUserService.getSystemUserList().subscribe({
       next: (res) => {
-        console.log(res.entity)
         this.dataSource = new MatTableDataSource(res.entity);
         this.dataSource.sort = this.sort;
         this.dataSource.paginator = this.paginator;
@@ -158,6 +75,7 @@ export class SytemUsersComponent implements OnInit {
       this.dataSource.paginator.firstPage();
     }
   }
+
   deleteSystemUser(id: number) {
     const confirmed = window.confirm(
       'Are you sure you want to delete this system user?'
@@ -167,7 +85,6 @@ export class SytemUsersComponent implements OnInit {
         next: (res) => {
           this._coreService.openSnackBar('System user deleted!', 'done');
           this.getSystemUserList();
-          
         },
         error: console.error, // Handle error appropriately
       });
@@ -185,5 +102,89 @@ export class SytemUsersComponent implements OnInit {
         }
       },
     });
+  }
+
+  generatePdf() {
+    const tableContainer = document.querySelector('.table-container');
+
+    if (tableContainer) {
+      html2canvas(tableContainer as HTMLElement).then((canvas) => {
+        const imgData = canvas.toDataURL('image/png');
+        const doc = new jsPDF({
+          orientation: 'landscape',
+          unit: 'mm',
+          format: 'a4'
+        });
+
+        const imgWidth = 280;
+        const pageHeight = 210;
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+        let heightLeft = imgHeight;
+        let position = 10;
+        const marginLeft = 5;
+        const marginRight = 3;
+
+        function addHeader() {
+          doc.setFontSize(12);
+          doc.text('System Users Report', imgWidth / 2, position, { align: 'center' });
+
+          const now = new Date();
+          const day = now.toLocaleDateString('en-US', { weekday: 'long' });
+          const date = now.toLocaleDateString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
+          const time = now.toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit' });
+
+          doc.setFontSize(6);
+          doc.text(`Printed on ${day} ${date} at ${time}`, imgWidth - marginRight, position, { align: 'right' });
+        }
+
+        function addFooter() {
+          doc.setFontSize(8);
+          doc.text('Elimu Pay Technologies | Copyright © 2024 | All rights reserved.', imgWidth / 2, doc.internal.pageSize.getHeight() - 5, { align: 'center' });
+        }
+
+        addHeader();
+        addFooter();
+        doc.addImage(imgData, 'PNG', marginLeft, position + 10, imgWidth - marginLeft - marginRight, imgHeight);
+        heightLeft -= pageHeight;
+
+        while (heightLeft >= 0) {
+          position = heightLeft - imgHeight + 10;
+          doc.addPage();
+          addHeader();
+          addFooter();
+          doc.addImage(imgData, 'PNG', marginLeft, position + 20, imgWidth - marginLeft - marginRight, imgHeight);
+          heightLeft -= pageHeight;
+        }
+
+        doc.save('System Users Report.pdf');
+      });
+    } else {
+      console.error('System users report container not found');
+    }
+  }
+
+  generateExcel(): void {
+    const fileName = 'System_Users_Report.xlsx';
+
+    // Extract data for Excel
+    const data = this.dataSource.data.map((row) => {
+      return {
+        'ID': row.id,
+        'First Name': row.first_name,
+        'Last Name': row.last_name,
+        'Role': row.usergroup,
+        'Email': row.email,
+        'School': row.school_name,
+        'Address': row.address,
+        'Nationality': row.nationality,
+      };
+    });
+
+    // Convert to Excel worksheet
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(data);
+    const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+
+    // Export to Excel file
+    XLSX.writeFile(workbook, fileName);
   }
 }
